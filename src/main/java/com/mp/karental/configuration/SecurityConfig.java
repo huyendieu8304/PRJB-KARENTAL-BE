fe617e735e9fd3cdc0bfa0148c46623981eec99a
Huyền Diệu <huyendieu8304@gmail.com>
2025-02-16 00:55:21 +0700

    fix CORS configuration

--- src/main/java/com/mp/karental/configuration/SecurityConfig.java
+++ src/main/java/com/mp/karental/configuration/SecurityConfig.java
@@ -60,21 +60,7 @@ public class SecurityConfig {
                                 //open public endpoints
                                 .requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS).permitAll()
                                 .anyRequest().authenticated()
-                ).cors(corsConfig -> corsConfig.configurationSource(new CorsConfigurationSource() {
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                        CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList("*")); //danh sách các origin được phép truy cập, các origin cách nhau bởi dấu ","
-                        config.setAllowedMethods(Collections.singletonList("*")); //cho phép các http method được truy cập. VD chỉ muốn cho phép các method get từ origin đã cho, nếu để dấu * là cho phép tất cả methods
-                        config.setAllowCredentials(true); //true là cho phép truyền credentials hoặc cookie khi gọi backend API
-                        config.setAllowedHeaders(Collections.singletonList("*")); //danh sách các headers mà backend chấp nhận từ UI app hoặc các origin khác. Dấu * là cho phép tất cả các loại request headers
-                        config.setExposedHeaders(Arrays.asList("Authorization"));
-                        config.setMaxAge(3600L); //cấu hình này sẽ tồn tại trong bao lâu (đơn vị second). Sau khi truy cập lần đầu tiên thì các lần sau sẽ không cần phải xét lại miễn là còn thời gian
-                        return config;
-                    }
-                }))
-
-                .csrf(AbstractHttpConfigurer::disable);
+                ).csrf(AbstractHttpConfigurer::disable);
 
 
         return http.build();
@@ -92,20 +78,26 @@ public class SecurityConfig {
      * @author DieuTTH4
      * @version 1.0
      */
-//    @Bean
-//    public CorsFilter corsFilter() {
-//
-//        CorsConfiguration corsConfiguration = new CorsConfiguration();
-//        corsConfiguration.addAllowedOrigin("http://localhost:3000"); //Allow this app to be accessed from localhost:3000
-//        corsConfiguration.addAllowedMethod("*"); //allow all method
-//        corsConfiguration.addAllowedHeader("*"); //allow all header
-//
-//        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-//        //apply CORS configuration for all endpoints in the application
-//        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-//
-//        return new CorsFilter(urlBasedCorsConfigurationSource);
-//    }
+    @Bean
+    public CorsFilter corsFilter() {
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //list origins allowed, separate by ,
+        corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+        //allow all methods
+        corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+        //true: allow credentials or cookies when call backend API
+        corsConfiguration.setAllowCredentials(true);
+
+        corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Authorization"));
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        //apply CORS configuration for all endpoints in the application
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
 
     /**
      * Configures the password encoder for the application.
