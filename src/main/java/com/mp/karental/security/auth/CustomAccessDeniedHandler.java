f58765e8bbd8b2b595593baf091beeb590cab0f7
huyendieu8304 <huyendieu8304@gmail.com>
2025-03-02 17:05:12 +0700

    add common method, get file extension and fix exception handler

--- src/main/java/com/mp/karental/security/auth/CustomAccessDeniedHandler.java
+++ src/main/java/com/mp/karental/security/auth/CustomAccessDeniedHandler.java
@@ -6,6 +6,7 @@ import com.mp.karental.exception.ErrorCode;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
@@ -21,12 +22,14 @@ import java.io.IOException;
  * @version 1.0
  */
 @Component
+@Slf4j
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request,
                        HttpServletResponse response,
                        AccessDeniedException accessDeniedException)
             throws IOException, ServletException {
+        log.info("Access denied, catch by AccessDeniedHandler");
         //User doesn't have permission to access
         ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
         response.setStatus(errorCode.getHttpStatusCode().value()); //Set the http status code
