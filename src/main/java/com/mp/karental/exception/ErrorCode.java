0c31f914da3a02cf2a2aeea5c84276a8921dcfd0
Ann4530 <anhhphe186087@fpt.edu.vn>
2025-03-31 15:56:47 +0700

    add log

--- src/main/java/com/mp/karental/exception/ErrorCode.java
+++ src/main/java/com/mp/karental/exception/ErrorCode.java
@@ -49,7 +49,7 @@ public enum ErrorCode {
     INVALID_IMAGE_FILE(2021, "Invalid image file type. Accepted formats are .jpg, .jpeg, .png", HttpStatus.BAD_REQUEST),
     INVALID_NATIONAL_ID(2022, "National ID must contain exactly 12 digits.", HttpStatus.BAD_REQUEST),
     NOT_UNIQUE_NATIONAL_ID(2023, "The national id already existed. Please try another national id", HttpStatus.BAD_REQUEST),
-    INVALID_CAR_STATUS_CHANGE(2024,"Car status must be NOT_VERIFIED.", HttpStatus.BAD_REQUEST),
+    INVALID_CAR_STATUS_CHANGE(2024,"Allowed transitions: NOT_VERIFIED → STOPPED, STOPPED → NOT_VERIFIED, VERIFIED → STOPPED", HttpStatus.BAD_REQUEST),
     INVALID_DATE_RANGE(2025, "Invalid date range. Pick-up date must be before drop-off date.", HttpStatus.BAD_REQUEST),
     INVALID_BOOKING_TIME(2026,"Invalid booking time", HttpStatus.BAD_REQUEST),
     INVALID_PAYMENT_TYPE(2027,"Payment can only WALLET/CASH/BANK_TRANSFER", HttpStatus.BAD_REQUEST),
