825510f2608c642b9bf3688faeb3c877d2f1d0c6
huyendieu8304 <huyendieu8304@gmail.com>
2025-04-03 10:14:21 +0700

    refactor LoggingConfig.java

--- src/main/java/com/mp/karental/logging/LoggingConfig.java
+++ src/main/java/com/mp/karental/logging/LoggingConfig.java
@@ -7,6 +7,7 @@ import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.ConsoleAppender;
 import ch.qos.logback.core.rolling.RollingFileAppender;
+import ch.qos.logback.core.rolling.SizeAndTimeBasedRollingPolicy;
 import ch.qos.logback.core.rolling.SizeBasedTriggeringPolicy;
 import ch.qos.logback.core.rolling.TimeBasedRollingPolicy;
 import ch.qos.logback.core.util.FileSize;
@@ -23,15 +24,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class LoggingConfig {
     private final int MAX_HISTORY = 7;
-    private final FileSize MAX_FILE_SIZE = FileSize.valueOf("10MB");
+    private final FileSize MAX_FILE_SIZE = FileSize.valueOf("15MB");
 
     public void setupLogging() {
         LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
-
-        //Connect to console
-//        ConsoleAppender<ILoggingEvent> consoleAppender = new ConsoleAppender<>();
-//        consoleAppender.setContext(context); //set up Logger Context
-//        consoleAppender.start();
 
         PatternLayoutEncoder fileEncoder = new PatternLayoutEncoder();
         fileEncoder.setContext(context);
@@ -42,30 +38,24 @@ public class LoggingConfig {
         RollingFileAppender<ILoggingEvent> fileAppender = new RollingFileAppender<>();
         fileAppender.setContext(context);
         fileAppender.setEncoder(fileEncoder);
-        fileAppender.setFile("logs/app.log"); //default logging file
 
         //setting time for rolling
-        TimeBasedRollingPolicy<ILoggingEvent> rollingPolicy = new TimeBasedRollingPolicy<>();
+        SizeAndTimeBasedRollingPolicy<ILoggingEvent> rollingPolicy = new SizeAndTimeBasedRollingPolicy<>();
         rollingPolicy.setContext(context);
         rollingPolicy.setParent(fileAppender); //connet to file appender
-        rollingPolicy.setFileNamePattern("logs/app.%d{yyyy-MM-dd}.log"); //naming for file when rolling
+        rollingPolicy.setFileNamePattern("logs/app.%d{yyyy-MM-dd}.%i.log"); //naming for file when rolling
+        rollingPolicy.setMaxFileSize(MAX_FILE_SIZE);
         rollingPolicy.setMaxHistory(MAX_HISTORY); //keep n day history, file older than 7 day will be deleted
+        rollingPolicy.setCleanHistoryOnStart(false); // Prevent deleting old logs on restart
         rollingPolicy.start();
 
-        //setting size for rolling
-        SizeBasedTriggeringPolicy<ILoggingEvent> triggeringPolicy = new SizeBasedTriggeringPolicy<>();
-        triggeringPolicy.setContext(context);
-        triggeringPolicy.setMaxFileSize(MAX_FILE_SIZE);
-        triggeringPolicy.start();
 
         //Assign policies to appender
         fileAppender.setRollingPolicy(rollingPolicy);
-        fileAppender.setTriggeringPolicy(triggeringPolicy);
         fileAppender.start();
 
-        //Get root logger and adding appenders
+        //Get root logger and adding appends
         Logger rootLogger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
-//        rootLogger.addAppender(consoleAppender);
         rootLogger.addAppender(fileAppender);
         rootLogger.setLevel(Level.INFO);
 
